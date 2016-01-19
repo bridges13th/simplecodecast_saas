a53628c62706540a8295dfeedc91723c74d253ed
bridges13th <bridges13th@gmail.com>
2016-01-19 02:30:31 +0000

    changed action of new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     def new
         #fill out user info
         @user = User.find(params[:user_id])
-        @profile = @user.build_profile
+        @profile = Profile.new
     end
     
     def create 
