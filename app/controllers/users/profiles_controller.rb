80c9daa90727735b2b8c316e5baeedc1dc9a6e53
bridges13th <bridges13th@gmail.com>
2016-01-21 06:40:17 +0000

    Removed hanging files

--- app/controllers/users/profiles_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProfilesController < ApplicationController
-    def new
-        #fill out user info
-        @user = User.find(params[:user_id])
-        @profile = @user.build_profile
-    end
-end
